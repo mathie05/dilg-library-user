6361b2044aa6fc32663ca45d1e6e1db79f6a7586
mathie05 <mathewadrianebriones05@gmail.com>
2024-05-01 15:03:27 +0800

    Firebase configuration and ReadME

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEosTgKtnHzbhdjo5NYSkd_OMITOm69U4",
-  authDomain: "fir-storage-630b5.firebaseapp.com",
-  projectId: "fir-storage-630b5",
-  storageBucket: "fir-storage-630b5.appspot.com",
-  messagingSenderId: "477538173797",
-  appId: "1:477538173797:web:bb517246c21d80c137578c",
-  measurementId: "G-6G8N192CXV"
+/* Get Firebase API Configuration by creating a Firebase Project at https://firebase.google.com/ */
 };
 
 const app = initializeApp(firebaseConfig);
